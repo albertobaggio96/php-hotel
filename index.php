6d216690c8fc8df21799525abddfc76b22ccd7f4
albertobaggio96 <albertobaggio96@gmail.com>
2023-01-25 17:15:53 +0100

    add a container

--- index.php
+++ index.php
@@ -49,69 +49,72 @@
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
 </head>
 <body>
-  <h1 class='fs-5'>Hotel list</h1>
+  <div class="container">
 
-  <form action="./index.php" method="GET">
-    <label for="parking-value">Seleziona se desideri il parcheggio :</label>
-    <select name="parkingValue" id="parking-value">
-      <option value="optional">opzionale</option>
-      <option value="yes">si</option>
-      <option value="no">no</option>
-    </select>
-    <label for="vote-filter">Valutazione minima</label>
-    <select name="voteFilter" id="vote-filter">
-      <option value="0">Tutti</option>
-      <option value="1">1</option>
-      <option value="2">2</option>
-      <option value="3">3</option>
-      <option value="4">4</option>
-      <option value="5">5</option>
-    </select>
-    <button type="submit">Cerca</button>
-  </form>
+    <h1>Hotel list</h1>
 
-  <div class="row">
+    <form action="./index.php" method="GET" class="mb-3">
+      <label for="parking-value">Seleziona se desideri il parcheggio :</label>
+      <select name="parkingValue" id="parking-value">
+        <option value="optional">opzionale</option>
+        <option value="yes">si</option>
+        <option value="no">no</option>
+      </select>
+      <label for="vote-filter">Valutazione minima</label>
+      <select name="voteFilter" id="vote-filter">
+        <option value="0">Tutti</option>
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+      </select>
+      <button type="submit">Cerca</button>
+    </form>
+
+    <div class="row">
+      <?php
+        foreach ($hotels[0] as $key => $hotel){
+          $key = str_replace('_', ' ', $key);
+          echo "<div class='col border border-secondary'> {$key} </div>";
+        }
+      ?>
+    </div>
+
     <?php
-      foreach ($hotels[0] as $key => $hotel){
-        $key = str_replace('_', ' ', $key);
-        echo "<div class='col border border-secondary'> {$key} </div>";
+      foreach ($hotels as $hotel){
+        echo "<div class='row'>";
+        $hotel["parking"] = $hotel["parking"] ? "si" : "no";
+        if(isset($_GET["parkingValue"]) && isset($_GET["voteFilter"])){
+          if ($hotel["vote"] >= $_GET["voteFilter"]){
+            if ($_GET["parkingValue"] === "optional"){
+              foreach ($hotel as $element){
+                echo "<div class='col border border-secondary'>{$element}</div>";
+              };
+            } elseif ($_GET["parkingValue"] === "yes"){
+              if($hotel["parking"] === "si"){
+                foreach ($hotel as $element){
+                  echo "<div class='col border border-secondary'>{$element}</div>";
+                };
+              }
+            } else{
+              if($hotel["parking"] === "no"){
+                foreach ($hotel as $element){
+                  echo "<div class='col border border-secondary'>{$element}</div>";
+                };
+              }
+            }
+          }
+        } else{
+          foreach ($hotel as $element){
+            $element = !$element ? "/" : $element;
+            echo "<div class='col border border-secondary'>{$element}</div>";
+          }
+        }
+        echo "</div>";
       }
     ?>
   </div>
-
-  <?php
-    foreach ($hotels as $hotel){
-      echo "<div class='row'>";
-      $hotel["parking"] = $hotel["parking"] ? "si" : "no";
-      if(isset($_GET["parkingValue"]) && isset($_GET["voteFilter"])){
-        if ($hotel["vote"] >= $_GET["voteFilter"]){
-          if ($_GET["parkingValue"] === "optional"){
-            foreach ($hotel as $element){
-              echo "<div class='col border border-secondary'>{$element}</div>";
-            };
-          } elseif ($_GET["parkingValue"] === "yes"){
-            if($hotel["parking"] === "si"){
-              foreach ($hotel as $element){
-                echo "<div class='col border border-secondary'>{$element}</div>";
-              };
-            }
-          } else{
-            if($hotel["parking"] === "no"){
-              foreach ($hotel as $element){
-                echo "<div class='col border border-secondary'>{$element}</div>";
-              };
-            }
-          }
-        }
-      } else{
-        foreach ($hotel as $element){
-          $element = !$element ? "/" : $element;
-          echo "<div class='col border border-secondary'>{$element}</div>";
-        }
-      }
-      echo "</div>";
-    }
-  ?>
   
 
 </body>
